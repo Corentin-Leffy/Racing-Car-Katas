b1cefbc1ea39fad654cfead66ce54846ced68e3d
Corentin Leffy <corentin.leffy@gmail.com>
2020-10-26 15:01:50 +0100

    :white_check_mark: Test the Alarm

--- kotlin/TirePressureMonitoringSystem/src/test/kotlin/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.kt
+++ kotlin/TirePressureMonitoringSystem/src/test/kotlin/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.kt
@@ -1,13 +1,45 @@
 package tddmicroexercises.tirepressuremonitoringsystem
 
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
-import org.junit.Assert.*
 
 class TestAlarm {
 
+    private val sensorWithHighPressure = TestableSensor(22.0)
+    private val sensorWithNormalPressure = TestableSensor(20.0)
+    private val sensorWithLowPressure = TestableSensor(16.0)
+
     @Test
-    fun foo() {
+    fun `Alarm is off at first`() {
         val alarm = Alarm()
-        assertEquals(false, alarm.isAlarmOn)
+        assertFalse(alarm.isAlarmOn)
     }
+
+    @Test
+    fun `Alarm is on when pressure is too low`() {
+        val alarm = Alarm(sensorWithLowPressure)
+        alarm.check()
+        assertTrue(alarm.isAlarmOn)
+    }
+
+    @Test
+    fun `Alarm is on when pressure is too high`() {
+        val alarm = Alarm(sensorWithHighPressure)
+        alarm.check()
+        assertTrue(alarm.isAlarmOn)
+    }
+
+    @Test
+    fun `Alarm is off when pressure is normal`() {
+        val alarm = Alarm(sensorWithNormalPressure)
+        alarm.check()
+        assertFalse(alarm.isAlarmOn)
+    }
+}
+
+class TestableSensor(
+    private val pressureValue: Double
+) : Sensor {
+    override fun popNextPressurePsiValue(): Double = pressureValue
 }
