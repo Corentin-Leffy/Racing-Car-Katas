202e260db55ec8a9a8ac2da3ddecd04d133db97a
Corentin Leffy <corentin.leffy@gmail.com>
2020-10-28 18:06:11 +0100

    :recycle: Refactor the class HtmlPagesConverter

--- kotlin/TextConverter/src/main/kotlin/tddmicroexercises/textconvertor/HtmlPagesConverter.kt
+++ kotlin/TextConverter/src/main/kotlin/tddmicroexercises/textconvertor/HtmlPagesConverter.kt
@@ -1,48 +1,15 @@
 package tddmicroexercises.textconvertor
 
-import java.io.BufferedReader
-import java.io.FileReader
 import java.io.IOException
-import java.util.ArrayList
+import java.io.StringReader
 
-class HtmlPagesConverter @Throws(IOException::class)
-constructor(val filename: String) {
-    private val breaks = ArrayList<Int>()
-
-    init {
-
-        this.breaks.add(0)
-        val reader = BufferedReader(FileReader(this.filename))
-        var cumulativeCharCount = 0
-        var line: String? = reader.readLine()
-        while (line != null) {
-            cumulativeCharCount += line.length + 1 // add one for the newline
-            if (line.contains("PAGE_BREAK")) {
-                val page_break_position = cumulativeCharCount
-                breaks.add(page_break_position)
-            }
-            line = reader.readLine()
-        }
-        reader.close()
-    }
+class HtmlPagesConverter(htmlContent: String) {
+    private val pages = htmlContent.split("\nPAGE_BREAK\n")
 
     @Throws(IOException::class)
     fun getHtmlPage(page: Int): String {
-        val reader = BufferedReader(FileReader(this.filename))
-        reader.skip(breaks[page].toLong())
-        val htmlPage = StringBuffer()
-        var line: String? = reader.readLine()
-        while (line != null) {
-            if (line.contains("PAGE_BREAK")) {
-                break
-            }
-            htmlPage.append(StringEscapeUtils.escapeHtml(line))
-            htmlPage.append("<br />")
-
-            line = reader.readLine()
-        }
-        reader.close()
-        return htmlPage.toString()
+        val htmlPage = pages[page]
+        val converter = HtmlTextConverter(StringReader(htmlPage))
+        return converter.convertToHtml()
     }
-
 }
--- kotlin/TextConverter/src/test/kotlin/tddmicroexercises/textconvertor/HtmlPagesConverterTest.kt
+++ kotlin/TextConverter/src/test/kotlin/tddmicroexercises/textconvertor/HtmlPagesConverterTest.kt
@@ -1,15 +1,55 @@
 package tddmicroexercises.textconvertor
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.IOException
-
-import org.junit.Assert.*
 
 class HtmlPagesConverterTest {
     @Test
     @Throws(IOException::class)
-    fun foo() {
-        val converter = HtmlPagesConverter("foo")
-        assertEquals("fixme", converter.filename)
+    fun `Convert ampersand symbol`() {
+        val converter = createHtmlPageConvertWithText("copy & paste")
+        assertEquals("copy &amp; paste<br />", converter.getHtmlPage(0))
+    }
+
+    @Test
+    fun `Convert less then symbol`() {
+        val converter = createHtmlPageConvertWithText("5 < 6")
+        assertEquals("5 &lt; 6<br />", converter.getHtmlPage(0))
+    }
+
+    @Test
+    fun `Convert greater then symbol`() {
+        val converter = createHtmlPageConvertWithText("10 > 3")
+        assertEquals("10 &gt; 3<br />", converter.getHtmlPage(0))
+    }
+
+    @Test
+    fun `Convert double quote`() {
+        val converter = createHtmlPageConvertWithText("\"Bonjour\"")
+        assertEquals("&quot;Bonjour&quot;<br />", converter.getHtmlPage(0))
+    }
+
+    @Test
+    fun `Convert single quote`() {
+        val converter = createHtmlPageConvertWithText("'Bonjour'")
+        assertEquals("&quot;Bonjour&quot;<br />", converter.getHtmlPage(0))
+    }
+
+    @Test
+    fun `Convert multiline`() {
+        val converter = createHtmlPageConvertWithText("test1\ntest2\ntest3")
+        assertEquals("test1<br />test2<br />test3<br />", converter.getHtmlPage(0))
+    }
+
+    @Test
+    fun `Split into two pages when PAGE_BREAK`() {
+        val converter = createHtmlPageConvertWithText("test1\nPAGE_BREAK\ntest2")
+        assertEquals("test1<br />", converter.getHtmlPage(0))
+        assertEquals("test2<br />", converter.getHtmlPage(1))
+    }
+
+    private fun createHtmlPageConvertWithText(text: String): HtmlPagesConverter {
+        return HtmlPagesConverter(text)
     }
 }
